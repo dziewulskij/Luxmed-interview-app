cf45da0c2da7f541cbf986575f53bdc941f1d684
dziewulskij <jaroslaw.dziewulski.kontakt@gmail.com>
2024-10-07 11:05:46 +0200

    Init app

---
+++ src/main/java/pl/dziewulskij/luxmedinterviewapp/LuxmedInterviewAppApplication.java
@@ -0,0 +1,13 @@
+package pl.dziewulskij.luxmedinterviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxmedInterviewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxmedInterviewAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dziewulskij/luxmedinterviewapp/LuxmedInterviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dziewulskij.luxmedinterviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxmedInterviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
